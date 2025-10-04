2fc28dc1377c51b77e45d84d1f574e70e757d26e
prado-hub <arturprad79@gmail.com>
2025-10-04 10:54:37 -0300

    feat(genre): adiciona dto para request e response

---
+++ src/main/java/com/jayjay/AniFlix/dto/request/GenreRequest.java
@@ -0,0 +1,4 @@
+package com.jayjay.AniFlix.dto.request;
+
+public record GenreRequest(String name) {
+}
---
+++ src/main/java/com/jayjay/AniFlix/dto/response/GenreResponse.java
@@ -0,0 +1,5 @@
+package com.jayjay.AniFlix.dto.response;
+
+public record GenreResponse(Long id,
+                            String name) {
+}
