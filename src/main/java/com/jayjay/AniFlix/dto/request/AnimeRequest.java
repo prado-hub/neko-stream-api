d813b752358a6b0d0346fc22019ffecc46f8a650
prado-hub <arturprad79@gmail.com>
2025-10-04 15:09:33 -0300

    feat(anime): adiciona dto para request e response

---
+++ src/main/java/com/jayjay/AniFlix/dto/request/AnimeRequest.java
@@ -0,0 +1,18 @@
+package com.jayjay.AniFlix.dto.request;
+
+import com.jayjay.AniFlix.entity.Genre;
+import com.jayjay.AniFlix.entity.Streaming;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record AnimeRequest(String title,
+                           String description,
+                           LocalDate releaseDate,
+                           double rating,
+                           LocalDateTime createdAt,
+                           LocalDateTime updatedAt,
+                           List<Genre> genres,
+                           List<Streaming> streamings) {
+}
---
+++ src/main/java/com/jayjay/AniFlix/dto/response/AnimeResponse.java
@@ -0,0 +1,19 @@
+package com.jayjay.AniFlix.dto.response;
+
+import com.jayjay.AniFlix.entity.Genre;
+import com.jayjay.AniFlix.entity.Streaming;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record AnimeResponse(Long Id,
+                            String title,
+                            String description,
+                            LocalDate releaseDate,
+                            double rating,
+                            LocalDateTime createdAt,
+                            LocalDateTime updatedAt,
+                            List<Genre> genres,
+                            List<Streaming> streamings) {
+}
