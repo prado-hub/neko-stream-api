8b282264b3b59395efe46ed5726eb1e18da9f560
prado-hub <arturprad79@gmail.com>
2025-10-03 21:15:22 -0300

    feat(streamings): adiciona endpoints para registro,atualização,deleção e listagem de streamings

---
+++ src/main/java/com/jayjay/AniFlix/controller/StreamingController.java
@@ -0,0 +1,42 @@
+package com.jayjay.AniFlix.controller;
+
+import com.jayjay.AniFlix.entity.Streaming;
+import com.jayjay.AniFlix.service.StreamingService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/streaming")
+public class StreamingController {
+
+    private StreamingService streamingService;
+
+    public StreamingController(StreamingService streamingService) {
+        this.streamingService = streamingService;
+    }
+
+    // get - list all streamings
+    @GetMapping("/")
+    public List<Streaming> listStreaming(){
+        return streamingService.listStreaming();
+    }
+
+    // post - register a new streaming
+    @PostMapping("/")
+    public Streaming createStreaming(@RequestBody Streaming streaming){
+        return streamingService.createStreaming(streaming);
+    }
+
+    // delete - delete a streaming
+    @DeleteMapping("/{id}")
+    public void deleteStreaming(@PathVariable Long id){
+       streamingService.deleteStreaming(id);
+    }
+
+    // update - update a streaming
+    @PutMapping("/{id}")
+    public Streaming updateStreaming(@PathVariable Long id,@RequestBody Streaming streamingAtt){
+        return streamingService.updateStreaming(id,streamingAtt);
+    }
+}
---
+++ src/main/java/com/jayjay/AniFlix/service/StreamingService.java
@@ -0,0 +1,41 @@
+package com.jayjay.AniFlix.service;
+
+import com.jayjay.AniFlix.entity.Genre;
+import com.jayjay.AniFlix.entity.Streaming;
+import com.jayjay.AniFlix.repository.StreamingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StreamingService {
+
+    private StreamingRepository streamingRepository;
+
+    public StreamingService(StreamingRepository streamingRepository) {
+        this.streamingRepository = streamingRepository;
+    }
+
+    public List<Streaming> listStreaming(){
+        return streamingRepository.findAll();
+    }
+
+    public Streaming createStreaming(Streaming streaming){
+        return streamingRepository.save(streaming);
+    }
+
+    public void deleteStreaming(Long id){
+        streamingRepository.deleteById(id);
+    }
+
+    public Streaming updateStreaming(Long id, Streaming streamingAtt){
+        Optional<Streaming> streamingVerify = streamingRepository.findById(id);
+        if(streamingVerify.isEmpty()){
+            return null;
+        }else{
+            streamingAtt.setId(id);
+            return streamingRepository.save(streamingAtt);
+        }
+    }
+}
