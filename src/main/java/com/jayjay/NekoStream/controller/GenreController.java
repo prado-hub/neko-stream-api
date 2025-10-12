dd55fa66199e0d131f721f303f059547fa6e8156
prado-hub <arturprad79@gmail.com>
2025-10-12 10:55:57 -0300

    feat(genre): adiciona ResponseEntity para respostas Http personalizadas

--- src/main/java/com/jayjay/NekoStream/controller/GenreController.java
+++ src/main/java/com/jayjay/NekoStream/controller/GenreController.java
@@ -3,6 +3,8 @@ package com.jayjay.NekoStream.controller;
 import com.jayjay.NekoStream.dto.request.GenreRequest;
 import com.jayjay.NekoStream.dto.response.GenreResponse;
 import com.jayjay.NekoStream.service.GenreService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,26 +22,27 @@ public class GenreController {
 
     //get - list all genre
     @GetMapping()
-    public List<GenreResponse> listGenres(){
-        return genreService.listGenre();
+    public ResponseEntity<List<GenreResponse>> listGenres(){
+        return ResponseEntity.ok(genreService.listGenre());
     }
 
     //get - list genre by id
     @GetMapping("/{id}")
-    public Optional<GenreResponse> listGenreById(@PathVariable Long id){
-        return genreService.listGenreById(id);
+    public ResponseEntity<Optional<GenreResponse>> listGenreById(@PathVariable Long id){
+        return ResponseEntity.ok(genreService.listGenreById(id));
     }
 
     // post - register a new genre
     @PostMapping()
-    public GenreResponse createGenre(@RequestBody GenreRequest genre){
-        return genreService.createGenre(genre);
+    public ResponseEntity<GenreResponse> createGenre(@RequestBody GenreRequest genre){
+        return ResponseEntity.status(HttpStatus.CREATED).body(genreService.createGenre(genre));
     }
 
     //delete - delete a genre
     @DeleteMapping("/{id}")
-    public void DeleteGenre(@PathVariable Long id){
+    public ResponseEntity<String> DeleteGenre(@PathVariable Long id){
         genreService.deleteGenre(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
